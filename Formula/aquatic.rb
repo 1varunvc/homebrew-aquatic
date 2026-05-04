ae1847ce9349c929950743d283d96e36d84b79c3
Varun Chawla <1varunvc@gmail.com>
2026-05-04 12:00:38 +0530

    fix: update sha256 after clean v0.1.0 re-tag

--- Formula/aquatic.rb
+++ Formula/aquatic.rb
@@ -2,7 +2,7 @@ class Aquatic < Formula
   desc "Modular macOS CLI toolkit for video processing, Git tagging, and data parsing"
   homepage "https://github.com/1varunvc/aquatic"
   url "https://github.com/1varunvc/aquatic/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "85e741dcc366761f99d3b7e0fb20fc48b576c56ac194e77a895fdf4b25885ab3"
+  sha256 "c359fb86237175e80ff3753ab05dd54d0c93b645383c726806b9e99d03a5ea09"
   license "GPL-3.0-or-later"
   version "0.1.0"
 

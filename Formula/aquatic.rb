eca6d44bc7f884a6e71b9e77a0c8b43af869b4eb
Varun Chawla <1varunvc@gmail.com>
2026-05-04 13:18:09 +0530

    bump: update sha256 for v0.1.0 re-release

--- Formula/aquatic.rb
+++ Formula/aquatic.rb
@@ -2,7 +2,7 @@ class Aquatic < Formula
   desc "Modular macOS CLI toolkit for video processing, Git tagging, and data parsing"
   homepage "https://github.com/1varunvc/aquatic"
   url "https://github.com/1varunvc/aquatic/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "c359fb86237175e80ff3753ab05dd54d0c93b645383c726806b9e99d03a5ea09"
+  sha256 "2b1f51b1b62a196bbe31c5dddaea6c8667c3711d3ba2e2f170f2b6cdf37536c6"
   license "GPL-3.0-or-later"
   version "0.1.0"
 
